c9cf2dcfc73f2e100223b6308354416ccb0f6dda
itwithlyam <64089164+itwithlyam@users.noreply.github.com>
2021-04-19 09:49:14 +0100

    Update NetSniff.ino

--- src/NetSniff.ino
+++ src/NetSniff.ino
@@ -1,21 +1,6 @@
-/*
-  WiFiAccessPoint.ino creates a WiFi access point and provides a web server on it.
-
-  Steps:
-  1. Connect to the access point "yourAp"
-  2. Point your web browser to http://192.168.4.1/H to turn the LED on or http://192.168.4.1/L to turn it off
-     OR
-     Run raw TCP "GET /H" and "GET /L" on PuTTY terminal with 192.168.4.1 as IP address and 80 as port
-
-  Created for arduino-esp32 on 04 July, 2018
-  by Elochukwu Ifediora (fedy0)
-*/
-
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <WiFiAP.h>
-
-#define LED_BUILTIN 2   // Set the GPIO pin where you connected your test LED or comment this line out if your dev board has a built-in LED
 
 // Set these to your desired credentials.
 const char *ssid = "IWL-S1";
@@ -126,7 +111,6 @@ void loop() {
           currentLine += c;      // add it to the end of the currentLine
         }
 
-        // Check to see if the client request was "GET /H" or "GET /L":
       }
     }
     // close the connection:
